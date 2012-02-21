00136adb8da0186e5e105d1b0d3b371c0eb82d80
txurdi <igor@txurdi.net>
2012-02-21 18:14:15 +0100

    Pasado test 3/53

--- kataBowling.php
+++ kataBowling.php
@@ -6,22 +6,40 @@ class kataBowling {
 		
 	}
 	
-	function puntua_frame ($frame) {
-		if ($frame=='-') return 0;
-		if ($frame=='X') return 10;
-		if (strlen($frame)==2) { 
-			if (substr($frame, 1,1)=='/') {
-				return 10;
-			} else {
-				return substr($frame, 0,1)+substr($frame, 1,1);
+	function puntua_frame ($frame, $tipo_anterior='') {
+		$resultado = 0;
+		$tipo = '';	
+		if ($frame=='-') {
+			$resultado = 0;
+		} else if ($frame=='X') {
+			$resultado = 10;
+			if ($tipo_anterior=='semipleno'){
+				$resultado+=10;
 			}
-	}
-		return $frame;
-		
+			$tipo = 'pleno';
+		} else if (strlen($frame)==2) { 
+			if (substr($frame, 1,1)=='/') {
+				$resultado = 10;
+				if ($tipo_anterior=='semipleno'){
+					$resultado+=10;
+				}
+				$tipo = 'semipleno';
+			} else {
+				$resultado = (substr($frame, 0,1)+substr($frame, 1,1));
+				if ($tipo_anterior=='semipleno'){
+					$resultado+=substr($frame, 0,1);
+				}
+			}
+		} else {
+			$resultado = $frame;
+		}
+		return array($resultado, $tipo);
 	}
 		
 	function resuelve($jugada) {
-		return ($this->puntua_frame(substr($jugada,0,2))+$this->puntua_frame(substr($jugada,2,2)));
+		$ar_frame1 = $this->puntua_frame(substr($jugada,0,2));
+		$ar_frame2 = $this->puntua_frame(substr($jugada,2,2),$ar_frame1[1]);
+		return $ar_frame1[0] + $ar_frame2[0];
 	}
 	
 	

a4f7439b8aa7bf7debed5a34417ac5f07ff01598
txurdi <igor@txurdi.net>
2012-02-21 17:49:40 +0100

    Pasado test 2345

--- kataBowling.php
+++ kataBowling.php
@@ -1,25 +1,27 @@
 <?php
-include_once "tablero.php";
-
 class kataBowling {
 	private $ar_resultado = array();
 	
 	function __construct() {
 		
 	}
-		
-	function resuelve($jugada) {
-		if ($jugada=='-') return 0;
-		if ($jugada=='X') return 10;
-		if (strlen($jugada)==2) { 
-			if (substr($jugada, 1,1)=='/') {
+	
+	function puntua_frame ($frame) {
+		if ($frame=='-') return 0;
+		if ($frame=='X') return 10;
+		if (strlen($frame)==2) { 
+			if (substr($frame, 1,1)=='/') {
 				return 10;
 			} else {
-				return substr($jugada, 0,1)+substr($jugada, 1,1);
+				return substr($frame, 0,1)+substr($frame, 1,1);
 			}
 	}
-		return $jugada;
+		return $frame;
 		
+	}
+		
+	function resuelve($jugada) {
+		return ($this->puntua_frame(substr($jugada,0,2))+$this->puntua_frame(substr($jugada,2,2)));
 	}
 	
 	

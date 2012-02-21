b7cdef8454e47ff8595a882145a647761f9c5c27
txurdi <igor@txurdi.net>
2012-02-21 17:44:06 +0100

    Pasado test 1/

--- kataBowling.php
+++ kataBowling.php
@@ -7,11 +7,17 @@ class kataBowling {
 	function __construct() {
 		
 	}
-	
+		
 	function resuelve($jugada) {
 		if ($jugada=='-') return 0;
 		if ($jugada=='X') return 10;
-		if (strlen($jugada)==2) return substr($jugada, 0,1)+substr($jugada, 1,1);
+		if (strlen($jugada)==2) { 
+			if (substr($jugada, 1,1)=='/') {
+				return 10;
+			} else {
+				return substr($jugada, 0,1)+substr($jugada, 1,1);
+			}
+	}
 		return $jugada;
 		
 	}
